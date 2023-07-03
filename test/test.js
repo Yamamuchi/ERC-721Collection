7902c9793ab5a08634b59334a1c473c3216d7961
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-03 18:24:22 +0100

    Added tests for ERC721 name/symbol

---
+++ test/test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+
+describe("BasicERC721", function() {
+
+    it("Should return correct name", async function () {
+
+        const tokenName = "MyERC721Token";
+
+        const BasicERC721 = await hre.ethers.getContractFactory("BasicERC721");
+        const BasicERC721Deployed = await BasicERC721.deploy(tokenName, "MERCT");
+
+        await BasicERC721Deployed.deployed();
+
+        expect(await BasicERC721Deployed.name()).to.equal(tokenName);
+    })
+
+    it("Should return correct name", async function () {
+
+        const tokenSymbol = "MERCT";
+
+        const BasicERC721 = await hre.ethers.getContractFactory("BasicERC721");
+        const BasicERC721Deployed = await BasicERC721.deploy("MyERC721Token", tokenSymbol);
+
+        await BasicERC721Deployed.deployed();
+
+        expect(await BasicERC721Deployed.symbol()).to.equal(tokenSymbol);
+    })
+
+}) 
