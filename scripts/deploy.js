c9f9795303a1d59c07de9bbabc38223022f0f613
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-03 18:39:36 +0100

    Basic deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,15 @@
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+
+  const BasicERC721 = await hre.ethers.getContractFactory("BasicERC721");
+  const BasicERC721Deployed = await BasicERC721.deploy("MyERC", "MERC");
+
+  await BasicERC721Deployed.deployed();
+
+  console.log("Deployed BasicERC721 to: ", BasicERC721Deployed.address);
+
+}
 
 main().catch((error) => {
   console.error(error);
