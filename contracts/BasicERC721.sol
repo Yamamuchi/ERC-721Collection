427009788a6ad435b823892cc2b1966a64dc0c3a
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-03 18:30:02 +0100

    console.log in .sol

--- contracts/BasicERC721.sol
+++ contracts/BasicERC721.sol
@@ -2,9 +2,11 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
 
 contract BasicERC721 is ERC721 {
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        console.log("Name:", name, "\nSymbol:", symbol);
     }
 }
