94d1d3ba038874d4f2158d09e92f42fea3a5d8a6
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-03 19:59:44 +0100

    Added network config + dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 // require('@nomiclabs/hardhat-ethers');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
