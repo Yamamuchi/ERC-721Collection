7ece4650dae6dc520080cbbbb693c54d55c6f492
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-04 19:15:56 +0100

    mint function + cost to mint

--- contracts/BasicERC721.sol
+++ contracts/BasicERC721.sol
@@ -5,10 +5,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract BasicERC721 is ERC721 {
+ 
+    uint256 public tokenSupply = 0;
+    // Immutables are stored directly in the deployed contract's bytecode (gas efficient)
+    uint256 public immutable MAX_SUPPLY;
+    uint256 public constant PRICE = 0.1 ether;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    string public baseURI;
+
+    constructor(string memory name, string memory symbol, uint256 _maxSupply) ERC721(name, symbol) {
+        MAX_SUPPLY = _maxSupply;
+
         console.log("Name:", name); 
         console.log("Symbol:", symbol);
         console.log("Deployed by:", msg.sender);
     }
-}
+
+    function mint() external payable {
+        require(tokenSupply < MAX_SUPPLY, "supply used up");
+        require(msg.value == PRICE, "wrong price");
+
+        _mint(msg.sender, tokenSupply);
+        tokenSupply++;
+    }
+
+    function viewBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function _baseURI() internal pure override returns(string memory) {
+        return "";
+    }
+} 
