6f45d9394a048f5b38db268084821ebd66dc3405
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-04 19:16:08 +0100

    verify contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,23 @@ const hre = require("hardhat");
 
 async function main() {
 
+  const tokenName = "MyERC";
+  const tokenSymbol = "ERC";
+
   const BasicERC721 = await hre.ethers.getContractFactory("BasicERC721");
-  const BasicERC721Deployed = await BasicERC721.deploy("MyERC", "MERC");
+  const BasicERC721Deployed = await BasicERC721.deploy(tokenName, tokenSymbol);
 
   await BasicERC721Deployed.deployed();
 
   console.log("Deployed BasicERC721 to: ", BasicERC721Deployed.address);
+
+  await hre.run("verify:verify", {
+    address: BasicERC721Deployed.address,
+    constructorArguments: [
+      tokenName,
+      tokenSymbol
+    ]
+  });
 
 }
 
