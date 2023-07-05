9185739cbf48ca19b63f5b0c4195bb3d9fb42a50
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-05 19:14:54 +0100

    ipfs hash update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,17 +2,17 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const tokenName = "MyERC";
-  const tokenSymbol = "ERC";
-  const baseURI = "ipfs://QmYfAwf2Ei75NZ5eSQP9sAvBi8nCsxkH5JaMVnexxw16mt/";
-  const contractURI = "ipfs://QmReuBxLqvPJ35ub42mC6mhecfb7wcaaV96ypD2DWpASn1"
+  const tokenName = "DiamondShades";
+  const tokenSymbol = "DS";
+  const baseURI = "ipfs://QmabUnb43Ltgs2HWNRN1VwnAtEQyGWKwc5XP3Wqz2tCbpn/";
+  const contractURI = "ipfs://Qma6edVgatxmt1AiEX7mCme2az2CNgSoNBkwRWcykj9MhC"
 
   const DiamondShades = await hre.ethers.getContractFactory("DiamondShades");
   const DiamondShadesDeployed = await DiamondShades.deploy(tokenName, tokenSymbol, baseURI, contractURI);
 
   await DiamondShadesDeployed.deployed();
 
-  console.log("Deployed BasicERC721 to: ", DiamondShadesDeployed.address);
+  console.log("Deployed DiamondShades to: ", DiamondShadesDeployed.address);
 
   await hre.run("verify:verify", {
     address: DiamondShadesDeployed.address,
